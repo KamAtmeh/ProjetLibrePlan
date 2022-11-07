f4bdccb1d59181e9675a571bdbd1458bc3c5a350
Sowltest <antoine.bouchez19@gmail.com>
2022-11-07 22:18:17 +0100

    yo les kheys

--- ProjetLibrePlan/src/main/java/org/ProjetLibrePlan/PageCreerTypeCritere.java
+++ ProjetLibrePlan/src/main/java/org/ProjetLibrePlan/PageCreerTypeCritere.java
@@ -3,7 +3,13 @@ package org.ProjetLibrePlan;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-public class PageCreerTypeCritere {
+public class PageCreerTypeCritere extends PageHeader {
+
+    PageRessourceListeFormulaireQualite pagetypeCritere = pagePlanification.clickOption("Ressources","Formulaires qualité", PageRessourceListeFormulaireQualite.class);
+        Thread.sleep(5000);
+        pagetypeCritere.clickBoutonCreerFormulaireQualite();
+        Thread.sleep(5000);
+
     //Xpath tableau
 
     @FindBy(xpath="//div[@class=\"clickable-rows z-grid\"]")
@@ -47,23 +53,42 @@ public class PageCreerTypeCritere {
     private WebElement champNom;
 
     @FindBy(xpath="//i[@class=\"z-combobox\"]")
-    private WebElement champType;
-
-    @FindBy(xpath="//td[@class=\"z-button-cm\"][contains(text(), \"Annuler\")]")
+    private WebElement champType; //voir plus tard pour les différents éléments
+//A CHANGER
+    @FindBy(xpath="//span[@title=\"Autoriser de multiples valeurs de ce type de critère dans la même période de temps\"]/../../following-sibling::td//input\"]")
     private WebElement caseValeurs;
 
-    @FindBy(xpath="//td[@class=\"z-button-cm\"][contains(text(), \"Annuler\")]")
+    @FindBy(xpath="//span[contains(text(), \"Hiérarchie\")]/../../following-sibling::td//input\"]")
     private WebElement caseHierarchie;
 
-    @FindBy(xpath="//td[@class=\"z-button-cm\"][contains(text(), \"Annuler\")]")
+    @FindBy(xpath="//span[contains(text(), \"Activé\")]/../../following-sibling::td//input\"]")
     private WebElement caseActive;
 
-    @FindBy(xpath="//td[@class=\"z-button-cm\"][contains(text(), \"Annuler\")]")
+    @FindBy(xpath="//textarea")
     private WebElement description;
 
-    //div[@class="z-row-cnt z-overflow-hidden"]/input[@class="z-textbox"]
+    //modif critère
+    @FindBy(xpath="//span[@title=\" Critère - Test bouton [Sauver et continuer]\"]/../../following-sibling::td//img[@src=\"/libreplan/common/img/ico_editar1.png\"]")
+    private WebElement boutonModif;
 
+    //titres page
+    @FindBy(xpath="//td[@class=\"z-caption-l\"]")
+    private WebElement titre;
 
+    //corbeille
+    @FindBy(xpath="//span[@title=\" Critère - Test bouton [Sauver et continuer]\"]/../../following-sibling::td//img[@src=\"/libreplan/common/img/ico_borrar1.png\"]")
+    private welement boutonSuppr;
 
+    @FindBy(xpath="//div[@class=\"z-window-modal z-window-modal-shadow\"]")
+    private WebElement confirmerSuppr;
+
+    @FindBy(xpath="//div[@class=\"z-window-modal z-window-modal-shadow\"]//td[contains(text(), \"OK\")]")
+    private WebElement okSuppr;
+
+    @FindBy(xpath="//div[@class=\"z-window-modal z-window-modal-shadow\"]//td[contains(text(), \"Annuler\")]")
+    private WebElement annulSuppr;
+
+    @FindBy(xpath="//div[@class=\"message_INFO\"]")
+    private WebElement confirmationSuppr;
 
 }
