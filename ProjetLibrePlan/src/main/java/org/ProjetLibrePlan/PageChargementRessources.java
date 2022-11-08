1458a988adb5efd3a2b250c37dd7936aeb72750b
Ngit11 <nl.nelly@yahoo.com>
2022-11-08 09:36:32 +0100

    PageBandeauLateral

---
+++ ProjetLibrePlan/src/main/java/org/ProjetLibrePlan/PageChargementRessources.java
@@ -0,0 +1,16 @@
+package org.ProjetLibrePlan;
+
+import org.openqa.selenium.WebDriver;
+
+public class PageChargementRessources extends PageHeader{
+
+
+    public PageChargementRessources(WebDriver driver) {
+        super(driver);
+    }
+
+
+
+
+
+}
