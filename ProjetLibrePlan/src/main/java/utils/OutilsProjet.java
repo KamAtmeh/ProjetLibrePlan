204044eebed065bb13fce68f98800f67aa3a491d
KamAtmeh <kamal.atmeh@hotmail.com>
2022-11-08 11:17:30 +0100

    cols method

--- ProjetLibrePlan/src/main/java/utils/OutilsProjet.java
+++ ProjetLibrePlan/src/main/java/utils/OutilsProjet.java
@@ -1,13 +1,17 @@
 package utils;
 
 import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 import java.io.File;
+import java.util.List;
 
 // Class for defining tools specific for this particular project
 public class OutilsProjet extends Logging {
+
+
 
 }
 
